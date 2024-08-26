7eb4edf359a0aef641d9d5578771bc884ee3da91
crawlic-stud <nikitosik0726@gmail.com>
2024-08-26 14:28:41 +0300

    feat: slider to images

--- src/app/catalog/views.py
+++ src/app/catalog/views.py
@@ -8,16 +8,54 @@ def index(request):
         "catalog.html",
         context={
             "items": [
-                {"name": "Туфля 3000"},
-                {"name": "Туфля GEL-KAYANO 28"},
-                {"name": "Туфля GEL-NIMBUS 24"},
-                {"name": "Туфля GEL-CUMULUS 24"},
-                {"name": "Туфля GEL-CONTEND 7"},
-                {"name": "Туфля GEL-EXCITE 9"},
-                {"name": "Туфля GEL-FLUX 5"},
-                {"name": "Туфля GEL-QUANTUM 360 6"},
-                {"name": "Туфля GEL-PULSE 13"},
-                {"name": "Туфля GEL-VENTURE 8"},
+                {
+                    "name": "Туфля 3000",
+                    "images": [
+                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
+                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
+                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
+                    ],
+                },
+                {
+                    "name": "Туфля GEL-KAYANO 28",
+                    "images": [
+                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
+                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
+                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
+                    ],
+                },
+                {
+                    "name": "Туфля GEL-NIMBUS 24",
+                    "images": [
+                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
+                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
+                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
+                    ],
+                },
+                {
+                    "name": "Туфля GEL-CUMULUS 24",
+                    "images": [
+                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
+                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
+                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
+                    ],
+                },
+                {
+                    "name": "Туфля GEL-CONTEND 7",
+                    "images": [
+                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
+                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
+                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
+                    ],
+                },
+                {
+                    "name": "Туфля GEL-EXCITE 9",
+                    "images": [
+                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
+                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
+                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
+                    ],
+                },
             ]
             * 10
         },
