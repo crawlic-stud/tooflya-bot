29620f5b59b0afe2bae54df14e373f89486875bd
crawlic-stud <nikitosik0726@gmail.com>
2024-08-27 10:31:18 +0300

    test with two cards

--- src/app/catalog/views.py
+++ src/app/catalog/views.py
@@ -1,4 +1,21 @@
+from pathlib import Path
 from django.shortcuts import render
+from PIL import Image
+
+images = [
+    "/static/images/sneaker1.jpg",
+    "/static/images/sneaker2.jpg",
+    "/static/images/sneaker3.jpg",
+]
+
+
+def compress_images():
+    for image in Path("src/app/static/images").glob("*.jpg"):
+        pil_image = Image.open(image)
+        pil_image.save(image, format="jpeg", quality=20)
+
+
+compress_images()
 
 
 # Create your views here.
@@ -8,54 +25,12 @@ def index(request):
         "catalog.html",
         context={
             "items": [
-                {
-                    "name": "Туфля 3000",
-                    "images": [
-                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
-                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
-                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
-                    ],
-                },
-                {
-                    "name": "Туфля GEL-KAYANO 28",
-                    "images": [
-                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
-                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
-                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
-                    ],
-                },
-                {
-                    "name": "Туфля GEL-NIMBUS 24",
-                    "images": [
-                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
-                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
-                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
-                    ],
-                },
-                {
-                    "name": "Туфля GEL-CUMULUS 24",
-                    "images": [
-                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
-                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
-                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
-                    ],
-                },
-                {
-                    "name": "Туфля GEL-CONTEND 7",
-                    "images": [
-                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
-                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
-                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
-                    ],
-                },
-                {
-                    "name": "Туфля GEL-EXCITE 9",
-                    "images": [
-                        "https://www.worldshop.eu/medias/img/8939992219678_w1500_z_274212826/adidas-lufthansa-limited-edition-sneaker-blue.jpeg",
-                        "https://www.gabor.com/cache-buster-1/thumbnails/Produkte/Schuhe/710168/997675/image-thumb__997675__product-slider-large/710168_A1.jpg",
-                        "https://balenciaga.dam.kering.com/m/612ece00eb41d4be/Medium-734734W3XL11090_F.jpg?v=2",
-                    ],
-                },
+                {"name": "Туфля 3000", "images": images},
+                {"name": "Туфля GEL-KAYANO 28", "images": images},
+                {"name": "Туфля GEL-NIMBUS 24", "images": images},
+                {"name": "Туфля GEL-CUMULUS 24", "images": images},
+                {"name": "Туфля GEL-CONTEND 7", "images": images},
+                {"name": "Туфля GEL-EXCITE 9", "images": images},
             ]
             * 10
         },
