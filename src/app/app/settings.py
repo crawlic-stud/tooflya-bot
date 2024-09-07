41179a6dd5a3adba928f7a7ca591850975777e40
crawlic-stud <nikitosik0726@gmail.com>
2024-09-07 12:07:18 +0300

    add load dotenv

--- src/app/app/settings.py
+++ src/app/app/settings.py
@@ -13,12 +13,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
