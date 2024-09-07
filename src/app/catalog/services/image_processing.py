9d4e2ed38c1f1eae9168d9fa9b50661813e10cf8
crawlic-stud <nikitosik0726@gmail.com>
2024-09-07 13:42:38 +0300

    fix: small refactor

--- src/app/catalog/services/image_processing.py
+++ src/app/catalog/services/image_processing.py
@@ -10,11 +10,14 @@ IMAGES_ROOT = "static/images/"
 STATIC_IMAGES_PATH = Path(IMAGES_ROOT)
 STATIC_IMAGES_PATH.mkdir(exist_ok=True, parents=True)
 
+_IMAGE_SIZE = [512.0, 512.0]
+_IMAGE_QUALITY = 85
+
 
 def optimize_and_save_image(image_bytes: io.BytesIO, filename: str):
     pil_image = Image.open(image_bytes)
-    pil_image.thumbnail([512.0, 512.0], Image.Resampling.LANCZOS)
+    pil_image.thumbnail(_IMAGE_SIZE, Image.Resampling.LANCZOS)
     new_filename = f'{filename.split(".")[0]}.webp'
     optimized_image_path = STATIC_IMAGES_PATH / new_filename
-    pil_image.save(optimized_image_path, format="webp", optimize=True, quality=85)
+    pil_image.save(optimized_image_path, format="webp", optimize=True, quality=_IMAGE_QUALITY)
     return IMAGES_URL + new_filename
